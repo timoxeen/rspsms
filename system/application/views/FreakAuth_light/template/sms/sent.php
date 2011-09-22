6a39d27babe544e779069335410c031c5a2a72b8
rampelli.pradeep <rampelli.pradeep@gmail.com>
2011-09-22 07:38:27 +0000

    sent sms list error

--- system/application/views/FreakAuth_light/template/sms/sent.php
+++ system/application/views/FreakAuth_light/template/sms/sent.php
@@ -13,8 +13,8 @@
 <?php 
 
 $toList = explode(';',$item->to_list);
-
-if($item->to_numbers != null || count($toList) <=1) :?>
+$toStatus = explode(',',$item->status);
+if($item->to_numbers != null || (count($toStatus) <=1)) :?>
 		<li>
 				<h3><a href="index.html"><?php echo ($item->to_numbers == null) ? $item->to_list : $item->to_numbers ?></a></h3>
 				<p><strong><?php if($item->status == 0) { echo '<font color="#720091">Pending</font>'; } else if($item->status == 1) { echo '<font color="#FFBC05">Sent</font>'; } else if($item->status == 2) { echo '<font color="#009100">Delivered</font>'; } else if($item->status == 3) { echo '<font color="#D1000A">May not be delivered(DND)</font>'; }else if($item->status == 4) { echo '<font color="#D1000A"> Failed (DND Number) </font>'; }?></strong></p>
@@ -26,7 +26,7 @@ if($item->to_numbers != null || count($toList) <=1) :?>
 
 <li>
 <?php 
-$toStatus = explode(',',$item->status);
+
 $conbined= array_combine($toList,$toStatus);
 
 $final = array();
