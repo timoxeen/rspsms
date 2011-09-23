cd38cbfd43260018586c4790581469b613f4a761
rampelli.pradeep@gmail.com <rampelli.pradeep@gmail.com@d5b337eb-7fb5-26f4-77cf-8eb46229ea69>
2011-09-23 11:20:57 +0000

    User angent detection

--- trunk/system/application/views/FreakAuth_light/template/sms/sent.php
+++ trunk/system/application/views/FreakAuth_light/template/sms/sent.php
@@ -10,56 +10,66 @@
 
 <li data-role="list-divider"><?php echo $item->ondate;?><span class="ui-li-count"></span></li>
 <?php endif;?>
-<?php 
-
-$toList = explode(';',$item->to_list);
-$toStatus = explode(',',$item->status);
-if($item->to_numbers != null || (count($toStatus) <=1)) :?>
-		<li>
-				<h3><a href="index.html"><?php echo ($item->to_numbers == null) ? $item->to_list : $item->to_numbers ?></a></h3>
+<?php
+$toList = explode(';', $item->to_list);
+$toStatus = explode(',', $item->status);
+if ($item->to_numbers != null || (count($toStatus) <= 1))
+	:
+?>
+		<li><?php  ?>
+				<h3><a href="#"><?php
+ if (empty ($item->to_numbers)) {
+		echo $item->to_list;
+	} else {
+		echo (empty ($item->name)) ?  $item->to_numbers : $item->name;
+	}
+?></a></h3>
 				<p><strong><?php if($item->status == 0) { echo '<font color="#720091">Pending</font>'; } else if($item->status == 1) { echo '<font color="#FFBC05">Sent</font>'; } else if($item->status == 2) { echo '<font color="#009100">Delivered</font>'; } else if($item->status == 3) { echo '<font color="#D1000A">May not be delivered(DND)</font>'; }else if($item->status == 4) { echo '<font color="#D1000A"> Failed (DND Number) </font>'; }?></strong></p>
-				<p><?php echo $item->message ?></p>
+				<p><pre><?php echo $item->message ?></pre></p>
 
 				<p class="ui-li-aside"><?php echo $item->attime;?></p>
 		</li>
 <?php else: ?>
 
 <li>
-<?php 
+<?php
 
-$conbined= array_combine($toList,$toStatus);
 
-$final = array();
-$sub2 = array();
-$sub1 = array();
-$sub3 = array();
-$sub4 = array();
-foreach($conbined as $key => $value)
-{
-	if($value == 2)
-	{
+$conbined = array_combine($toList, $toStatus);
+
+$final = array ();
+$sub2 = array ();
+$sub1 = array ();
+$sub3 = array ();
+$sub4 = array ();
+foreach ($conbined as $key => $value) {
+	if ($value == 2) {
 		$sub2[] = $key;
 	}
-	if($value == 1)
-	{
+	if ($value == 1) {
 		$sub1[] = $key;
 	}
-	if($value == 3)
-	{
+	if ($value == 3) {
 		$sub3[] = $key;
 	}
-	if($value == 4)
-	{
+	if ($value == 4) {
 		$sub4[] = $key;
-	}	
+	}
 }
-$final[1] = implode(',',$sub1);
-$final[2] = implode(',',$sub2);
-$final[3] = implode(',',$sub3);
-$final[4] = implode(',',$sub4);
 
+$final[1] = implode(',', $sub1);
+$final[2] = implode(',', $sub2);
+$final[3] = implode(',', $sub3);
+$final[4] = implode(',', $sub4);
 ?>
-				<h3><a href="#"><?php echo ($item->to_numbers == null) ? $item->to_list : $item->to_numbers ?></a></h3>
+				<h3><a href="#"><?php
+				 if (empty ($item->to_numbers)) {
+		echo $item->to_list;
+	} else {
+		echo (empty ($item->name)) ?  $item->to_numbers : $item->name;
+	}
+	
+?></a></h3>
 				
 				<?php foreach($final as $key => $numbers): ?>
 				<?php if(!empty($numbers)) : ?>
@@ -67,7 +77,7 @@ $final[4] = implode(',',$sub4);
 				<p><?php echo $numbers; ?></p>
 				<?php endif;?>
 				<?php endforeach; ?>
-				<p><?php echo $item->message ?></p>
+				<p><pre><?php echo $item->message ?></pre></p>
 				<p class="ui-li-aside"><?php echo $item->attime;?></p>
 </li>
 <?php endif; ?>
